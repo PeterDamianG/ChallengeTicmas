ee2b66ea7ba33bba96827c4d1991ee746d01e1ab
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-08 20:45:22 -0300

    fix: add env var to get img from api.

--- src/constants/enviroment.ts
+++ src/constants/enviroment.ts
@@ -3,6 +3,10 @@
  */
 export const API_URL = process.env.REACT_APP_API_URL;
 /**
+ * Get env const API_URL to set a url for make requests.
+ */
+export const API_IMG = process.env.REACT_APP_API_IMG;
+/**
  * Get env const API_KEY to set a key for use in requests.
  */
 export const API_KEY = process.env.REACT_APP_API_KEY;
