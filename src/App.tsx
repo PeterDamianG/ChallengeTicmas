7f278391cd61a4da894b6122ca147a8a2752be9e
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-08 15:55:12 -0300

    feat: make a base components for the proyect.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Container } from '@chakra-ui/react';
+import InputSearch from 'components/inputsearch/InputSearch';
+import ContainerGenre from 'components/movieslist/container/ContainerGenre';
+/**
+ *
+ * @returns
+ */
 export default function App() {
-  return <div className='App'>asdasdas</div>;
+  return (
+    <>
+      <Container centerContent p={8} maxW='container.md'>
+        <InputSearch />
+      </Container>
+      <ContainerGenre />
+    </>
+  );
 }
