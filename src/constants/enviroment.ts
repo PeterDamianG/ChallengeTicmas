a412fca97777d3ec5a3c9008faa5c9b03b35bd5a
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-08 17:06:21 -0300

    feat: add new env var to limit chars in search.

--- src/constants/enviroment.ts
+++ src/constants/enviroment.ts
@@ -6,3 +6,7 @@ export const API_URL = process.env.REACT_APP_API_URL;
  * Get env const API_KEY to set a key for use in requests.
  */
 export const API_KEY = process.env.REACT_APP_API_KEY;
+/**
+ * Get env const LIMIT_SEARCH to set a limit of characters to use in searchs.
+ */
+export const LIMIT_SEARCH = Number(process.env.REACT_APP_LIMIT_SEARCH);
