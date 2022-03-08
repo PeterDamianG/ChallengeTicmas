95436c07d4ba781d1882a62f0a00bb21f7fb706d
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-08 18:03:30 -0300

    feat: middleware to check status in requests movies.

---
+++ src/components/middlewares/CheckMovies.tsx
@@ -0,0 +1,43 @@
+import useGetMovies from 'hooks/useGetMovies';
+import { Spinner, Heading } from '@chakra-ui/react';
+import ContainerGenre from 'components/movieslist/container/ContainerGenre';
+/**
+ * Declare type/interface for this component.
+ */
+type CheckMoviesProps = {
+  search: string;
+};
+/**
+ * Component middleware for show feedback to user about proccess of get movies.
+ * @param {string} search - String to use in request for searching.
+ * @example
+ * import CheckMovies from 'components/middlewares/CheckMovies';
+ * ...
+ *   <CheckMovies search={search} />
+ * ...
+ */
+export default function CheckMovies({ search }: CheckMoviesProps) {
+  const { movies, isLoading, isError } = useGetMovies(search);
+  // If search is loading.
+  if (isLoading) {
+    return (
+      <Spinner
+        thickness='4px'
+        speed='0.65s'
+        emptyColor='gray.200'
+        color='blue.500'
+        size='xl'
+      />
+    );
+  }
+  // If we have a error.
+  if (isError) {
+    return (
+      <Heading as='h3'>
+        We are having a problem. Please contact support. We are sorry.
+      </Heading>
+    );
+  }
+  // If all is good give info to next component.
+  return <ContainerGenre data={movies.results} />;
+}
