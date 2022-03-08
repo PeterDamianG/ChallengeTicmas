7f5a42a91f460bfdfdb1fcab48c505d1faf79b38
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-08 17:03:22 -0300

    feat: add setting and config for env vars.

---
+++ src/constants/enviroment.ts
@@ -0,0 +1,8 @@
+/**
+ * Get env const API_URL to set a url for make requests.
+ */
+export const API_URL = process.env.REACT_APP_API_URL;
+/**
+ * Get env const API_KEY to set a key for use in requests.
+ */
+export const API_KEY = process.env.REACT_APP_API_KEY;
