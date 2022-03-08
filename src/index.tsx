b207f0a6c0932f62ddf8406d489ebf308ecae46f
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-08 12:47:58 -0300

    fix: add comment.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-
+/**
+ * Entry point of app. Set strictmode and chakra provider.
+ */
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
