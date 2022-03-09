cd2afb294335585715efbae0a50b807db097f029
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-08 22:50:54 -0300

    feat: make a component like a container for accordion list.

--- src/components/movieslist/container/ContainerGenre.tsx
+++ src/components/movieslist/container/ContainerGenre.tsx
@@ -1,29 +1,75 @@
-import { Accordion } from '@chakra-ui/react';
-import ItemList from 'components/movieslist/itemlist/ItemList';
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Heading,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react';
+import { movie } from 'types/movie';
+import groupByGenres from 'utils/groupByGenres';
+import ItemCard from 'components/movieslist/itemcard/ItemCard';
+import listGenre from './listGenre';
 /**
  * Declare type/interface for this component.
  */
 type ContainerGenreProps = {
-  genre?: string;
+  data: movie[];
 };
 /**
- *
- * @param param0
- * @returns
+ * Container component to render accordion list.
+ * @param {movie[]} data - List of movies getting by request.
+ * @example
+ * import ContainerGenre from 'components/movieslist/container/ContainerGenre';
+ * ...
+ *   return <ContainerGenre data={movies.results} />;
+ * ...
  */
-export default function ContainerGenre({
-  genre = 'Unknown',
-}: ContainerGenreProps) {
+export default function ContainerGenre({ data }: ContainerGenreProps) {
+  // Run a reducer to order by group.
+  const setGroupByGenres = groupByGenres(data);
+  // Make arrays to renders.
+  const arrGroup = Object.keys(setGroupByGenres).map((key) => [
+    listGenre[key],
+    setGroupByGenres[key],
+  ]);
+  // Return component.
   return (
     <Accordion
       allowMultiple
-      m={6}
-      borderWidth='4px'
+      ml={16}
+      mr={16}
+      borderWidth='6px'
       borderRadius='lg'
       overflow='hidden'
     >
-      {[0, 1, 2].map((value) => (
-        <ItemList key={value} genre={genre + value} />
+      {/* Render items/categories/genres groups. */}
+      {arrGroup.map((value: [string, movie[]]) => (
+        <AccordionItem key={value[0]}>
+          <AccordionButton _expanded={{ bg: 'blue.500', color: 'white' }}>
+            <Box flex='1' textAlign='left'>
+              <Heading as='h3'>{value[0]}</Heading>
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <Wrap spacing='4%' justify='space-around'>
+              {/* Render card movies. */}
+              {value[1].map((m: movie) => (
+                <WrapItem key={m.id}>
+                  <ItemCard
+                    imageUrlPoster={m.poster_path}
+                    title={m.title}
+                    year={m.release_date}
+                  />
+                </WrapItem>
+              ))}
+            </Wrap>
+          </AccordionPanel>
+        </AccordionItem>
       ))}
     </Accordion>
   );
