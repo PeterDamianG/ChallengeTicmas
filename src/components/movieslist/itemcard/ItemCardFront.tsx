c35631a93f01d4dab0f70b069b4bc1c821ccfbc5
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-09 00:27:08 -0300

    feat: make component to show card movie.

---
+++ src/components/movieslist/itemcard/ItemCardFront.tsx
@@ -0,0 +1,94 @@
+import { Box, Heading, Text, Stack, Image } from '@chakra-ui/react';
+import { API_IMG } from 'constants/enviroment';
+/**
+ * Declare type/interface for this component.
+ */
+type ItemCardProps = {
+  imageUrlPoster: string;
+  title: string;
+  year: string;
+  imageAlt?: string;
+};
+/**
+ * Component to render front card movies.
+ * @example
+ * import ItemCardFront from './ItemCardFront';
+ * ...
+ *   <ItemCardFront
+ *     imageUrlPoster={imageUrlPoster}
+ *     title={title}
+ *     year={year}
+ *     imageAlt={imageAlt}
+ *   />
+ * ...
+ */
+export default function ItemCardFront({
+  imageUrlPoster,
+  title,
+  year,
+  imageAlt = 'The image is not working. We are sorry.',
+}: ItemCardProps) {
+  return (
+    <Box
+      p={6}
+      maxW='350px'
+      w='full'
+      bg='blue.500'
+      boxShadow='2xl'
+      rounded='lg'
+      pos='relative'
+      zIndex={1}
+      _hover={{
+        background: 'blue.700',
+        transition: 'background 0.4s linear',
+      }}
+    >
+      <Box
+        rounded='lg'
+        mt={-12}
+        pos='relative'
+        height='230px'
+        _after={{
+          transition: 'all .3s ease',
+          content: '""',
+          w: 'full',
+          h: 'full',
+          pos: 'absolute',
+          top: 5,
+          left: 0,
+          backgroundImage: `url(${API_IMG}${imageUrlPoster})`,
+          filter: 'blur(15px)',
+          zIndex: -1,
+        }}
+        _groupHover={{
+          _after: {
+            filter: 'blur(20px)',
+          },
+        }}
+      >
+        <Image
+          rounded='lg'
+          height={230}
+          width={282}
+          objectFit='cover'
+          src={`${API_IMG}${imageUrlPoster}`}
+          alt={imageAlt}
+          fallbackSrc='https://i.ytimg.com/vi/q41AIk3gPKQ/hqdefault.jpg'
+        />
+      </Box>
+      <Stack pt={10} align='center'>
+        <Text color='gray.200' fontSize='sm' textTransform='uppercase'>
+          &bull; {year} &bull;
+        </Text>
+        <Heading
+          color='white'
+          fontSize='2xl'
+          fontFamily='body'
+          fontWeight={500}
+        >
+          {title}
+        </Heading>
+      </Stack>
+    </Box>
+  );
+}
