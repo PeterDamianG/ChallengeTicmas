7a064d7678284bd30cb318652fc929c79b9b02f1
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-09 14:30:01 -0300

    fix: add docs.

--- src/utils/groupByGenres.ts
+++ src/utils/groupByGenres.ts
@@ -3,7 +3,9 @@ import { movie } from 'types/movie';
  * Make a fuction reduce to get movies separate by group.
  * @param {movie} data - Filter data type movie getting for request.
  * @example
- *
+ * import groupByGenres from 'utils/groupByGenres';
+ * const setGroupByGenres = groupByGenres(data);
+ * @returns Object {[number],[arr[X]}
  */
 export default function groupByGenres(data: movie[]) {
   return data.reduce((genres, actualMovie) => {
