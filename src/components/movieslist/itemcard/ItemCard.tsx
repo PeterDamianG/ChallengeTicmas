509519f2c4eaf8d28201e6ff022d3cb8e05ee10c
Peter Damián Gómez <peterdamiang@gmail.com>
2022-03-08 22:52:53 -0300

    feat: card for list, base version. Check to fix style.

--- src/components/movieslist/itemcard/ItemCard.tsx
+++ src/components/movieslist/itemcard/ItemCard.tsx
@@ -1,23 +1,34 @@
 import { Box, Container, Image } from '@chakra-ui/react';
-
+import { API_IMG } from 'constants/enviroment';
+/**
+ * Declare type/interface for this component.
+ */
+type ItemCardProps = {
+  imageUrlPoster: string;
+  title: string;
+  year: string;
+  imageAlt?: string;
+};
 // Sample card from Airbnb
-export default function ItemCard() {
-  const property = {
-    imageUrlPoster:
-      'https://image.tmdb.org/t/p/w200/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg',
-    imageUrlBackdrop:
-      'https://image.tmdb.org/t/p/w200/nNmJRkg8wWnRmzQDe2FwKbPIsJV.jpg',
-    imageAlt: 'The image is not working. We are sorry.',
-    title: 'The Avengers',
-    year: '2012-04-25',
-  };
-
+export default function ItemCard({
+  imageUrlPoster,
+  title,
+  year,
+  imageAlt = 'The image is not working. We are sorry.',
+}: ItemCardProps) {
   return (
-    <Box maxW='lg' borderWidth='4px' borderRadius='lg' overflow='hidden'>
-      <Image src={property.imageUrlPoster} alt={property.imageAlt} />
-      <Container centerContent p={4} maxW='container.md'>
-        <Box fontWeight='semibold' as='h4' lineHeight='tight' isTruncated>
-          {property.title}
+    <Box maxW='md' borderWidth='6px' borderRadius='lg'>
+      <Image
+        src={`${API_IMG}${imageUrlPoster}`}
+        alt={imageAlt}
+        fit='cover'
+        align='center'
+        w='100%'
+        fallbackSrc='https://i.ytimg.com/vi/q41AIk3gPKQ/hqdefault.jpg'
+      />
+      <Container centerContent p={4} isTruncated>
+        <Box fontWeight='semibold' as='h4' lineHeight='tight'>
+          {title}
         </Box>
         <Box
           color='gray.500'
@@ -26,7 +37,7 @@ export default function ItemCard() {
           fontSize='xs'
           textTransform='uppercase'
         >
-          &bull; {property.year} &bull;
+          &bull; {year} &bull;
         </Box>
       </Container>
     </Box>
